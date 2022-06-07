e5569780dad608db5110d40b017b6fd37a535ff7
Abdelrahman mostafa <a.abdo.mae@gmail.com>
2022-06-07 02:43:07 +0200

    config

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ const orderRoute = require('./routes/orderRoutes');
 const app = express();
 //! 1)security
 
+//CORS handler
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === "OPTIONS") {
+      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+      return res.status(200).json({});
+    }
+    next();
+  });
+  
 
 //! 2) middlewares
 //? body paser, for reading data from the body
