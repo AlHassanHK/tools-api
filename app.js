e33112f30448b1b07c0172341f61ac407fe82cad
AlHassanHK <al.hassan.elkady@gmail.com>
2022-12-18 08:44:16 +0200

    fix mongoose validation error

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
 const groceries = require("./models/ordersModel");
 const express = require("express");
 const app = express();
-//! 1)security
 
-//CORS handler
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
@@ -31,34 +31,12 @@ app.post("/createGroceryItem", async (req, res) => {
   try {
     const newGroceryItem = await groceries.create(req.body);
     res.status(201).json({
-      status: "Successfully created order.",
-      data: { newOrder }
+      status: "Successfully created item.",
+      data: { newGroceryItem }
     });
   } catch (error) {
     res.status(400).send(error.message);
   }
 });
-
-// async (req, res) => {
-//   //won't take status, check that item exists and is in stock in the other service
-//   try {
-//     const newOrder = await orders.create(req.body);
-//     res.status(201).json({
-//       status: "Successfully created order.",
-//       data: { newOrder },
-//     });
-//   } catch (error) {
-//     res.status(400).json({
-//       status: "Bad request.",
-//       data: {},
-//     });
-//   }
-// };
-
-//! 2) middlewares
-//? body paser, for reading data from the body
-app.use(express.json());
-//? to pass data coming from URL encoded form
-app.use(express.urlencoded({ extended: true }));
 
 module.exports = app;
